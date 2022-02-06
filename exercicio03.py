a92245ba16013fadae70aeaa6d7643d402e864c3
Willams Junior <juniormodern@hotmail.com>
2022-02-06 19:21:12 -0300

    exercicio01
    o famoso hello world

---
+++ exercicio03.py
@@ -0,0 +1,7 @@
+from tkinter.tix import InputOnly
+
+
+print('Soma de dois numeros')
+n1=int(input('digite o primeiro numero: '))
+n2= int(input('Digite o segundo numero: '))
+print('a soma dos numeros {} e {} é {}'.format(n1, n2, (n1+n2)))
---
+++ exercicio04.py
@@ -0,0 +1,7 @@
+print('programa soma de media')
+n1=float(input('Digite nota 1: '))
+n2=float(input('Digite nota 2: '))
+n3=float(input('Digite nota 3: '))
+n4=float(input('Digite nota 4: '))
+print('a sua media é {}'.format((n1+n2+n3+n4)/2))
+
