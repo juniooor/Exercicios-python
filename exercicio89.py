7728cc5c4903bc33fdf744d2fa49f395f51781ac
Willams Junior <juniormodern@hotmail.com>
2022-02-26 22:54:49 -0300

    exercicio funcao

--- exercicio89.py
+++ exercicio89.py
@@ -1,1 +1,11 @@
 #aça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
+print('CALCULADORA DE AREA DE TERRENO')
+
+def area(a, b):
+    ar=a*b
+    print(f'A àrea do terreno {a} x {b} é de {ar}m²')
+
+
+larg=float(input('largura do terreno: '))
+comp=float(input('Comprimento do terreno: '))
+area(larg,comp)
