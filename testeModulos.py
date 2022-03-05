fdd1bb766d5f29eac967488156ab20a21c175eb5
Willams Junior <juniormodern@hotmail.com>
2022-03-04 23:10:11 -0300

    exercicios modulos

---
+++ testeModulos.py
@@ -0,0 +1,7 @@
+import moduti
+
+num=int(input('Digite um numero: '))
+f=moduti.fatorial(num)
+print(f'O fatorial de {num} é {f}')
+print(f'o dobro de {num} é {moduti.dobro(num)}')
+print(f'o triplo de {num} é {moduti.triplo(num)}')
